01debfee96fef0ecd0a317262aa59016ba13b602
7000WORDS <emekadrake707@gmail.com>
2021-04-03 19:24:24 +0100

    minor changes

--- main.py
+++ main.py
@@ -9,44 +9,51 @@ x = random.choice(game)
 g = (x)
 
 
+print("""
+the rules of the game are:
+rock beats scissors
+scissors beats paper
+paper beats rock
+""")
 
-print("enter either 'rock', 'paper' or 'scissors' ")
-play = input("")
+def main():
 
-if play == "scissors":
-    print(g)
-    if g == "scissors":
-        print('you got a draw')
-    elif g == "paper":
-        print("you won")
-    elif g == "rock":
-        print("you lost")
-    else:
-        print('not allowed such input')
+    print("enter either 'rock', 'paper' or 'scissors' ")
+    play = input("")
 
-
-elif play == "rock":
-    print(g)
-    if g == "rock":
-        print('you got a draw')
-    elif g == "paper":
-        print("you lost")
-    elif g == "scissors":
-        print("you won")
-    else:
-        print("not allowed such input")
+    if play == "scissors":
+        print(g)
+        if g == "scissors":
+            print('you got a draw')
+        elif g == "paper":
+            print("you won")
+        elif g == "rock":
+            print("you lost")
+        else:
+            print('not allowed such input')
 
 
-elif play == "paper":
-    print(g)
-    if g == "paper":
-        print('you got a draw')
-    elif g == "scissors":
-        print("you lost")
-    elif g == "rock":
-        print("you won")
-    else:
-        print('not allowed such input')
+    elif play == "rock":
+        print(g)
+        if g == "rock":
+            print('you got a draw')
+        elif g == "paper":
+            print("you lost")
+        elif g == "scissors":
+            print("you won")
+        else:
+            print("not allowed such input")
 
 
+    elif play == "paper":
+        print(g)
+        if g == "paper":
+            print('you got a draw')
+        elif g == "scissors":
+            print("you lost")
+        elif g == "rock":
+            print("you won")
+        else:
+            print('not allowed such input')
 
+main()
